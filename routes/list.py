3cdf26d088bb3e29ba8c944904c198e5b060bbf7
Kazuki Tobita <kazukiti201@gmail.com>
2023-04-28 23:43:36 +0900

    Modify list endpoints

--- routes/list.py
+++ routes/list.py
@@ -1,21 +1,25 @@
 from fastapi import APIRouter
-from schemas.list import ListAddResponse, ListSearchResponse
+from schemas.list import ListAddRequest, ListAddResponse, ListSearchResponse
 
 
 router = APIRouter(prefix="/list", tags=["list"])
 
 
 @router.post("", response_model=ListAddResponse)
-def add():
+def add(req: ListAddRequest):
     """
     リーディングリストに追加
     """
-    return {"message": "pong"}
+    # TODO: URLからタイトルを取得する
+    # TODO: URLからラベルを生成する
+    # TODO: URL、タイトル、ラベル、ステータス、追加/更新日時をDBに保存する
+    return {"message": req.url}
 
 
 @router.get("", response_model=ListSearchResponse)
-def search():
+def search(label: str):
     """
     リーディングリストの検索
     """
-    return {"message": "pong"}
+    # TODO: ラベルで検索する
+    return {"message": label}
--- schemas/list.py
+++ schemas/list.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel
 
 
+class ListAddRequest(BaseModel):
+    url: str
+
+
 class ListAddResponse(BaseModel):
     message: str
 
