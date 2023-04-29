c416ce9b7b9baceefdccdec5ed9d37a5846abdc4
Kazuki Tobita <kazukiti201@gmail.com>
2023-04-29 17:37:42 +0900

    Modify db settings

--- db/settings.py
+++ db/settings.py
@@ -1,22 +1,23 @@
 from functools import lru_cache
 
 from dotenv import load_dotenv
-from pydantic import BaseSettings
-
-
-load_dotenv()
+from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    ADDRESS: str
-    PORT: int
-    USERNAME: str
-    PASSWORD: str
+    ADDRESS: str = Field("localhost", env="DB_ADDRESS")
+    PORT: int = Field(27017, env="DB_PORT")
+    USERNAME: str = Field("root", env="DB_USERNAME")
+    PASSWORD: str = Field("password", env="DB_PASSWORD")
 
     class Config:
         env_file: str = ".env"
 
+    def __init__(self, *args, **kwargs):
+        load_dotenv()
+        super().__init__(*args, **kwargs)
 
-@lru_cache
-def get_settings():
-    return Settings()
+    @classmethod
+    @lru_cache
+    def get_settings(cls):
+        return Settings()
