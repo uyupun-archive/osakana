f08216d808181de647df901ea30e5ff1b2561bb4
Kazuki Tobita <kazukiti201@gmail.com>
2023-04-29 21:15:29 +0900

    Modify name

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI, APIRouter
 
-from api.routes import ping, list
+from api.routes import ping, reading_list
 from deps import get_settings
 
 
@@ -13,7 +13,7 @@ def init_app(app: FastAPI) -> FastAPI:
 def register_routes(app: FastAPI) -> None:
     router = APIRouter(prefix="/api")
     router.include_router(router=ping.router)
-    router.include_router(router=list.router)
+    router.include_router(router=reading_list.router)
     app.include_router(router=router)
 
 
