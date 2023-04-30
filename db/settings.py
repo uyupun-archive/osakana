01d2cf129999e7c93e425aa2520305d54d2fa4b4
Kazuki Tobita <kazukiti201@gmail.com>
2023-04-30 22:10:23 +0900

    Modify settings

--- db/settings.py
+++ db/settings.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from functools import lru_cache
 
 from dotenv import load_dotenv
@@ -6,7 +7,6 @@ from pydantic import BaseSettings, Field
 
 class Settings(BaseSettings):
     DB_NAME: str
-    DB_COLLECTION_NAME: str
     ADDRESS: str = Field("localhost", env="DB_ADDRESS")
     PORT: int = Field(27017, env="DB_PORT")
     USERNAME: str = Field("root", env="DB_USERNAME")
@@ -15,11 +15,11 @@ class Settings(BaseSettings):
     class Config:
         env_file: str = ".env"
 
-    def __init__(self, *args, **kwargs):
-        load_dotenv()
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        load_dotenv()
 
     @classmethod
-    @lru_cache
-    def get_settings(cls):
+    @lru_cache(maxsize=1)
+    def get_settings(cls) -> Settings:
         return Settings()
--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from functools import lru_cache
 
 from dotenv import load_dotenv
@@ -14,11 +15,11 @@ class Settings(BaseSettings):
     class Config:
         env_file: str = ".env"
 
-    def __init__(self, *args, **kwargs):
-        load_dotenv()
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        load_dotenv()
 
     @classmethod
-    @lru_cache
-    def get_settings(cls):
+    @lru_cache(maxsize=1)
+    def get_settings(cls) -> Settings:
         return Settings()
