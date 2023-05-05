7f7914c08b7aa54e77c25b8690aa0f7a46ed4bcb
Kazuki Tobita <kazukiti201@gmail.com>
2023-05-06 01:56:00 +0900

    Modify migration

--- db/migrations/_01_create_reading_list_collection.py
+++ db/migrations/_01_create_reading_list_collection.py
@@ -8,14 +8,15 @@ class CreateReadingListCollectionMigrator(BaseMigrator):
     def __init__(self, reading_list_record: Type[ReadingListRecord]=ReadingListRecord) -> None:
         super().__init__()
         self._name = "01_create_reading_list_collection"
-        self._index_name = reading_list_record.get_name()
+        self._reading_list_record = reading_list_record
+        self._index_name = self._reading_list_record.get_name()
 
     def get_name(self) -> str:
         return self._name
 
     def up(self) -> None:
         self._db_client.create_index(index_name=self._index_name)
-        attribute = ReadingListRecord.has_field(field="updated_at")
+        attribute = self._reading_list_record.has_field(field="updated_at")
         self._db_client.sortable(index_name=self._index_name, attribute=attribute)
 
     def down(self) -> None:
