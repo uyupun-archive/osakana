86de8e0b4276bbb1abc2aefa7f93bd126e13aaec
Kazuki Tobita <kazukiti201@gmail.com>
2023-05-06 15:58:06 +0900

    Add get document

--- db/repos/reading_list.py
+++ db/repos/reading_list.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
 import random
 from uuid import UUID
-from zoneinfo import ZoneInfo
 
 from db.client import Document
 from db.models.reading_list import ReadingListRecord
 from db.repos.base import BaseRepository
-from lib.timezone import get_timezone
 
 
 class ReadingListRepository(BaseRepository):
@@ -22,11 +20,11 @@ class ReadingListRepository(BaseRepository):
         )
 
     def find(self, id: UUID) -> ReadingListRecord:
-        documents = self._db_client.search_documents(
+        document = self._db_client.get_document(
             index_name=self._index_name,
-            keyword=str(id)
+            id=id
         )
-        reading_list = ReadingListRecord.convert_instance(document=documents[0])
+        reading_list = ReadingListRecord.convert_instance(document=document)
         return reading_list
 
     def search(self, keyword: str) -> list[ReadingListRecord]:
@@ -60,7 +58,7 @@ class ReadingListRepository(BaseRepository):
         )[0]
         return document
 
-    def read(self, reading_list_record: ReadingListRecord, timezone: ZoneInfo=get_timezone()) -> None:
+    def read(self, reading_list_record: ReadingListRecord) -> None:
         reading_list_record.update_timestamp()
         reading_list_record.read()
         document = ReadingListRecord.convert_dict(reading_list_record=reading_list_record)
