3463a5a29313a6028e42a292ca85c11dc9510623
Kazuki Tobita <kazukiti201@gmail.com>
2023-05-07 19:18:29 +0900

    Add ui

--- dashboard/src/routes/home/index.js
+++ dashboard/src/routes/home/index.js
@@ -4,6 +4,34 @@ const Home = () => {
 	return (
 		<div>
 			<img src="../../assets/logo-with-text.svg" alt="Osakana logo with text" width="300" />
+			<div>
+				<input type="text" placeholder="https://..." />
+				<button type="button">Add</button>
+			</div>
+			<div>
+				<input type="text" placeholder="Keyword" />
+				<button type="button">Search</button>
+				<button type="button">Feeling</button>
+			</div>
+			<table border="1">
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							<a href="https://preactjs.com/tutorial/" target="_blank" rel="noreferrer noopener">TestTestTestTestTest</a>
+						</td>
+						<td>
+							<button type="button">Read</button>
+							<button type="button">Delete</button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 			<section>
 				<Resource
 					title="Learn Preact"
