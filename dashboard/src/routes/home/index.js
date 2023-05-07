be6af5778a353ea7fcc8ee2f698fcdb75cb228ac
Kazuki Tobita <kazukiti201@gmail.com>
2023-05-07 19:26:57 +0900

    Modify ui

--- dashboard/src/routes/home/index.js
+++ dashboard/src/routes/home/index.js
@@ -21,34 +21,24 @@ const Home = () => {
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>
-							<a href="https://preactjs.com/tutorial/" target="_blank" rel="noreferrer noopener">TestTestTestTestTest</a>
-						</td>
-						<td>
-							<button type="button">Read</button>
-							<button type="button">Delete</button>
-						</td>
-					</tr>
+					<ReadingListRecord title="TestTestTestTestTest" url="https://preactjs.com/tutorial/" />
 				</tbody>
 			</table>
-			<section>
-				<Resource
-					title="Learn Preact"
-					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-					link="https://preactjs.com/tutorial/"
-				/>
-			</section>
 		</div>
 	);
 };
 
-const Resource = props => {
+const ReadingListRecord = props => {
 	return (
-		<a href={props.link}>
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
+		<tr>
+			<td>
+				<a href={props.url} target="_blank" rel="noreferrer noopener">{props.title}</a>
+			</td>
+			<td>
+				<button type="button">Read</button>
+				<button type="button">Delete</button>
+			</td>
+		</tr>
 	);
 };
 
