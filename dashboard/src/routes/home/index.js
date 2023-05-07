fb56a32ad5aaf7491a2c19ce38e09c0c17c5ec06
Kazuki Tobita <kazukiti201@gmail.com>
2023-05-07 17:10:14 +0900

    Modify home

--- dashboard/src/routes/home/index.js
+++ dashboard/src/routes/home/index.js
@@ -3,25 +3,13 @@ import { h } from 'preact';
 const Home = () => {
 	return (
 		<div>
-			<a href="https://preactjs.com">
-				<img src="../../assets/logo.svg" alt="Preact Logo" height="160" width="160" />
-			</a>
-			<h1>Get Started Building PWAs with Preact-CLI</h1>
+			<img src="../../assets/logo.svg" alt="Osakana Logo" height="160" width="160" />
+			<h1>Osakana</h1>
 			<section>
 				<Resource
 					title="Learn Preact"
 					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
 					link="https://preactjs.com/tutorial/"
-				/>
-				<Resource
-					title="Differences to React"
-					description="If you're coming from React, check out our docs for where Preact differs"
-					link="https://preactjs.com/guide/v10/differences-to-react"
-				/>
-				<Resource
-					title="Learn Preact-CLI"
-					description="To learn more about Preact-CLI, read through the ReadMe & Wiki"
-					link="https://github.com/preactjs/preact-cli#preact-cli--"
 				/>
 			</section>
 		</div>
