c1c4cb7689f5428df16b94738183a572b01e6a72
Kazuki Tobita <kazukiti201@gmail.com>
2023-05-08 02:23:29 +0900

    Add ReadingListRecord component

--- dashboard/src/pages/home/home.tsx
+++ dashboard/src/pages/home/home.tsx
@@ -1,3 +1,4 @@
+import { FunctionalComponent } from 'preact'
 import { useEffect, useState } from 'preact/hooks'
 import { ping } from '../../api/ping'
 import LogoWithText from '../../assets/logo-with-text.svg'
@@ -33,17 +34,28 @@ export function Home() {
 					</tr>
 				</thead>
         <tbody>
-          <tr>
-            <td>
-              <a href="https://preactjs.com/tutorial/" target="_blank" rel="noreferrer noopener">TestTestTestTestTest</a>
-            </td>
-            <td>
-              <button type="button" onClick={() => console.log("Read")}>Read</button>
-              <button type="button" onClick={() => console.log("Delete")}>Delete</button>
-            </td>
-          </tr>
+          <ReadingListRecord title="TestTestTestTestTest" url="https://preactjs.com/tutorial/" />
 				</tbody>
 			</table>
     </>
   )
 }
+
+interface ReadingListRecordProps {
+  title: string;
+  url: string;
+};
+
+const ReadingListRecord: FunctionalComponent<ReadingListRecordProps> = (props) => {
+  return (
+    <tr>
+      <td>
+        <a href={props.url} target="_blank" rel="noreferrer noopener">{props.title}</a>
+      </td>
+      <td>
+        <button type="button" onClick={() => console.log("Read")}>Read</button>
+        <button type="button" onClick={() => console.log("Delete")}>Delete</button>
+      </td>
+    </tr>
+  );
+};
