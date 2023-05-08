b44a8ea84700dc15dffd66f1d3e9d5b7cd750938
Kazuki Tobita <kazukiti201@gmail.com>
2023-05-09 01:45:26 +0900

    Add type guard

--- dashboard/src/api/readingList.ts
+++ dashboard/src/api/readingList.ts
@@ -1,16 +1,21 @@
 import axios from 'axios';
 
-import type { ReadingList, ReadingListRecord, ReadingListSearchParams } from '../types';
+import type { ReadingList, ReadingListRecord, ReadingListRecordResponse, ReadingListSearchParams } from '../types';
+import { isValidReadingListRecordResponse } from '../types';
 
 export const searchReadingList = async (params: ReadingListSearchParams): Promise<ReadingList> => {
   const res = await axios.get('/api/reading-list', {
     params: params
   });
-  return _parseReadingList(res.data);
+
+  if (Array.isArray(res.data) && res.data.every(isValidReadingListRecordResponse)) {
+    return res.data.map(_parseReadingListRecord);
+  }
+  throw new Error('Invalid response');
 };
 
-const _parseReadingList = (data: any): ReadingList => {
-  const readingList = data.map((record: any): ReadingListRecord => ({
+const _parseReadingListRecord = (record: ReadingListRecordResponse): ReadingListRecord => {
+  return {
     id: record.id,
     url: record.url,
     title: record.title,
@@ -19,6 +24,5 @@ const _parseReadingList = (data: any): ReadingList => {
     createdAt: new Date(record.created_at),
     updatedAt: new Date(record.updated_at),
     readAt: record.read_at ? new Date(record.read_at) : null,
-  }));
-  return readingList;
+  };
 };
--- dashboard/src/types/readingList.ts
+++ dashboard/src/types/readingList.ts
@@ -1,11 +1,36 @@
-import type { UUID, HttpUrl } from "./common";
+import type { Uuid, HttpUrl } from "./common";
+import { isUuid4, isHttpUrl, isIso8601 } from '../types/common';
 
 export interface ReadingListSearchParams {
   keyword: string;
 }
 
+export interface ReadingListRecordResponse {
+  id: Uuid;
+  url: HttpUrl;
+  title: string;
+  is_read: boolean;
+  thumb: HttpUrl | null;
+  created_at: string;
+  updated_at: string;
+  read_at: string | null;
+};
+
+export const isValidReadingListRecordResponse = (record: any): record is ReadingListRecordResponse => {
+  return (
+    isUuid4(record.id) &&
+    isHttpUrl(record.url) &&
+    typeof record.title === 'string' &&
+    typeof record.is_read === 'boolean' &&
+    (record.thumb === null || isHttpUrl(record.thumb)) &&
+    isIso8601(record.created_at) &&
+    isIso8601(record.updated_at) &&
+    (record.read_at === null || isIso8601(record.read_at))
+  );
+};
+
 export interface ReadingListRecord {
-  id: UUID;
+  id: Uuid;
   url: HttpUrl;
   title: string;
   isRead: boolean;
