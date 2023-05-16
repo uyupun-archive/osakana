9d88ce487943732abd9bed600c59478a6d89cb04
Kazuki Tobita <kazukiti201@gmail.com>
2023-05-17 03:50:39 +0900

    Add CORS middleware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes import ping, reading_list
 from errors.handlers import register_error_handlers
@@ -15,6 +16,16 @@ def init_app(settings: Settings=Settings.get_settings()) -> FastAPI:
     return app
 
 
+def register_middleware(app: FastAPI, settings: Settings=Settings.get_settings()) -> None:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[settings.ALLOWED_ORIGIN],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
 def register_routes(app: FastAPI) -> None:
     router = APIRouter(prefix="/api")
     router.include_router(router=ping.router)
@@ -27,6 +38,7 @@ def run_app(app: FastAPI, settings: Settings=Settings.get_settings()) -> None:
 
 
 app = init_app()
+register_middleware(app=app)
 register_routes(app=app)
 register_error_handlers(app=app)
 
