307251dab48cc75f8514fac36769a81e1182175b
Kazuki Tobita <kazukiti201@gmail.com>
2023-05-17 03:51:11 +0900

    Delete proxy

--- dashboard/src/api/endpoints/ping.ts
+++ dashboard/src/api/endpoints/ping.ts
@@ -4,8 +4,10 @@ import type { PingResponse } from '../types';
 import { isValidPingResponse } from '../types';
 import { PingTypeError } from '../errors';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const ping = async (): Promise<PingResponse> => {
-  const res = await axios.get('/api/ping');
+  const res = await axios.get(`${apiUrl}/api/ping`);
   if (isValidPingResponse(res.data)) {
     return res.data;
   }
--- dashboard/src/api/endpoints/readingList.ts
+++ dashboard/src/api/endpoints/readingList.ts
@@ -16,12 +16,14 @@ import {
   ReadingListRecordNotYetReadError
 } from '../errors';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const addReadingListRecord = async (url: HttpUrl): Promise<void> => {
   if (!isHttpUrl(url)) {
     throw new InvalidHttpUrlError();
   }
   try {
-    await axios.post('/api/reading-list', {url});
+    await axios.post(`${apiUrl}/api/reading-list`, {url});
   } catch (e: unknown) {
     if (e instanceof AxiosError) {
       if (e.response?.status === StatusCodes.NOT_FOUND) {
@@ -37,7 +39,7 @@ export const addReadingListRecord = async (url: HttpUrl): Promise<void> => {
 };
 
 export const searchReadingList = async (keyword: string): Promise<ReadingList> => {
-  const res = await axios.get('/api/reading-list', {params: {keyword}});
+  const res = await axios.get(`${apiUrl}/api/reading-list`, {params: {keyword}});
 
   if (Array.isArray(res.data) && res.data.every(isValidReadingListRecordResponse)) {
     return res.data.map(_parseReadingListRecord);
@@ -46,7 +48,7 @@ export const searchReadingList = async (keyword: string): Promise<ReadingList> =
 };
 
 export const fetchFeelingReadingListRecord = async (): Promise<ReadingListRecord> => {
-  const res = await axios.get('/api/reading-list/feeling');
+  const res = await axios.get(`${apiUrl}/api/reading-list/feeling`);
 
   if (isValidReadingListRecordResponse(res.data)) {
     return _parseReadingListRecord(res.data);
@@ -59,7 +61,7 @@ export const readReadingListRecord = async (id: Uuid4): Promise<void> => {
     throw new InvalidUuid4Error();
   }
   try {
-    await axios.patch(`/api/reading-list/read/${id}`);
+    await axios.patch(`${apiUrl}/api/reading-list/read/${id}`);
   } catch (e: unknown) {
     if (e instanceof AxiosError) {
       if (e.response?.status === StatusCodes.NOT_FOUND) {
@@ -79,7 +81,7 @@ export const unreadReadingListRecord = async (id: Uuid4): Promise<void> => {
     throw new InvalidUuid4Error();
   }
   try {
-    await axios.patch(`/api/reading-list/unread/${id}`);
+    await axios.patch(`${apiUrl}/api/reading-list/unread/${id}`);
   } catch (e: unknown) {
     if (e instanceof AxiosError) {
       if (e.response?.status === StatusCodes.NOT_FOUND) {
@@ -99,7 +101,7 @@ export const deleteReadingListRecord = async (id: Uuid4): Promise<void> => {
     throw new InvalidUuid4Error();
   }
   try {
-    await axios.delete(`/api/reading-list/${id}`);
+    await axios.delete(`${apiUrl}/api/reading-list/${id}`);
   } catch (e: unknown) {
     if (e instanceof AxiosError) {
       if (e.response?.status === StatusCodes.NOT_FOUND) {
@@ -116,7 +118,7 @@ export const bookmarkReadingListRecord = async (id: Uuid4): Promise<void> => {
     throw new InvalidUuid4Error();
   }
   try {
-    await axios.patch(`/api/reading-list/bookmark/${id}`);
+    await axios.patch(`${apiUrl}/api/reading-list/bookmark/${id}`);
   } catch (e: unknown) {
     if (e instanceof AxiosError) {
       if (e.response?.status === StatusCodes.NOT_FOUND) {
