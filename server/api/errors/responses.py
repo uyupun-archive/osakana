83f9585619fe6e551b268677570ccf4bb40a77df
Kazuki Tobita <kazukiti201@gmail.com>
2023-05-20 05:07:01 +0900

    Add status code validator

--- server/api/errors/responses.py
+++ server/api/errors/responses.py
@@ -1,13 +1,19 @@
 from typing import Any
 
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from starlette.status import HTTP_403_FORBIDDEN, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT, HTTP_422_UNPROCESSABLE_ENTITY
 
 
 class ApiError(BaseModel):
     status_code: int
     message: str
+
+    @validator("status_code")
+    def validate_status_code(cls, status_code: int):
+        if status_code < 400 or status_code > 599:
+            raise ValueError()
+        return status_code
 
     def response(self):
         return JSONResponse(status_code=self.status_code, content=self.dict())
