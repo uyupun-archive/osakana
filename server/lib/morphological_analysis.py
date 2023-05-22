eb82ee403e8ddb821bc39a6c3e4a94dabecbf0ef
Kazuki Tobita <kazukiti201@gmail.com>
2023-05-22 23:12:30 +0900

    Add text cleansing

--- server/lib/morphological_analysis.py
+++ server/lib/morphological_analysis.py
@@ -1,3 +1,4 @@
+import re
 import sys
 
 from sudachipy import Dictionary, SplitMode
@@ -6,9 +7,15 @@ from sudachipy import Dictionary, SplitMode
 class MorphologicalAnalysisService:
     @classmethod
     def generate(cls, text: str) -> list[str]:
+        preprocessed_text = cls._cleansing(text=text)
         sudachi = Dictionary().create()
-        morphemes = [m.surface() for m in sudachi.tokenize(text, mode=SplitMode.C)]
+        morphemes = [m.surface() for m in sudachi.tokenize(text=preprocessed_text, mode=SplitMode.C)]
         return morphemes
+
+    @classmethod
+    def _cleansing(cls, text: str) -> str:
+        preprocessed_text = re.sub(pattern=r"[^\w]", repl="", string=text)
+        return preprocessed_text
 
 
 if __name__ == "__main__":
--- server/lib/ngrams.py
+++ server/lib/ngrams.py
@@ -1,15 +1,22 @@
+import re
 import sys
 
 
 class NgramService:
     @classmethod
     def generate(cls, text: str, n: int) -> list[str]:
-        words = text.split(" ")
+        preprocessed_text = cls._cleansing(text=text)
+        words = preprocessed_text.split(" ")
         ngrams = []
         for word in words:
             for i in range(len(word) - n + 1):
                 ngrams.append(word[i:i+n])
         return ngrams
+
+    @classmethod
+    def _cleansing(cls, text: str) -> str:
+        preprocessed_text = re.sub(pattern=r"[^\w\s]", repl="", string=text)
+        return preprocessed_text
 
 
 if __name__ == "__main__":
